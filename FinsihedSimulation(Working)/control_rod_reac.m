3d2e965f05477fe9499ade771cb72ce5579ff1bd
chiken-bake <161653750+chiken-bake@users.noreply.github.com>
2024-06-07 11:39:23 -0700

    Add files via upload

--- FinsihedSimulation(Working)/control_rod_reac.m
+++ FinsihedSimulation(Working)/control_rod_reac.m
@@ -1,8 +1,8 @@
 function cr_reac_eff = control_rod_reac(u,wierd,o,p) % CAN BE FURTHER REFINED, roughly dtermines reactivity; tables with values will later be added for better accuracy
-    reg_rod=linspace(0,3.19, 1001);
-    safety_rod=linspace(0,2.15,1001);
-    shim_rod=linspace(0,2.56,1001);
-    trans_rod=linspace(0,2.68,1001);
+    reg_rod=linspace(0,0.25, 1001);
+    safety_rod=linspace(0,0.17,1001);
+    shim_rod=linspace(0,0.20,1001);
+    trans_rod=linspace(0,0.21,1001);
     if u>100
         u=100;
     end
@@ -21,6 +21,6 @@ function cr_reac_eff = control_rod_reac(u,wierd,o,p) % CAN BE FURTHER REFINED, r
     trans_height=u;
    
     %cr_reac_eff = -1*(old_Mod_Reactivity_R+old_Fuel_Reactivity_R);
-    cr_reac_eff=(reg_rod(1001-reg_height*10)+safety_rod(1001-(safety_height*10))+shim_rod(1001-shim_height*10)+trans_rod(1001-trans_height*10))*0.0075;
+    cr_reac_eff=((reg_rod(1+reg_height*10)+safety_rod((1+safety_height*10))+shim_rod(1+shim_height*10)+trans_rod(1+trans_height*10)))*0.0075;
 
 end 
